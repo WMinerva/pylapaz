784a51610fb624dbd1c69962957926310697d13d
WMinerva <nicolas.ochoa1697@gmail.com>
2024-05-21 20:03:23 -0400

    navic & barbecue

--- 01.py
+++ 01.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+def foo():
+    print("foo")
+    print("Hello World!")
+    for i in range(10):
+        print(i)
+    j = 10
+    while j > 0:
+        print(j)
+        j -= 1
